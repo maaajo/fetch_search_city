c4b37828e927956c5f3058a715cb245a0c76961f
Mateusz Majchrzak <36929127+maaajo@users.noreply.github.com>
2019-03-26 11:40:47 +0100

    Solved problem with async, await && added highlighting

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
-'use strict';
+"use strict";
 
-function getData() {
-	const arrCities = [];
+async function getPromise() {
 	const endpoint =
-		'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
-	fetch(endpoint).then((resp) => resp.json()).then((data) => arrCities.push(...data));
-	return arrCities;
+		'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';	
+	const response = await fetch(endpoint);
+	const data = await response.json();
+	return data;
 }
 
 function findMatches(data, subset) {
@@ -27,28 +27,36 @@ function displayResults(filteredData, element) {
 		removeAllChildrens(element);
 	} else {
 		filteredData.forEach(function(item) {
-			const numberFormatted = formatNumberToCurrency(+item.population);
-			const row = `<li class="suggestion"><span>${item.city}, ${item.state}</span><span>${numberFormatted}</span></li>`;
+			const regEx = new RegExp(element.value, 'gi');
+			const cityState = `${item.city}, ${item.state}`;
+			const highlight = cityState.replace(regEx, '<span class="highlight">' + element.value + '</span>');
+			const row = `<li class="suggestion"><span>${highlight}</span><span>${item.population}</span></li>`;
 			hitsArr.push(row);
 		});
 	}
 	list.innerHTML = hitsArr.join('');
 }
 
-function formatNumberToCurrency(number) {
+function formatNumber(number) {
 	const formatter = new Intl.NumberFormat();
 	return formatter.format(number);
 }
 
-const data = getData();
+function formatObjectNumber(data) {
+	data.forEach( function(item) {item.population = formatNumber(+item.population)});
+	return data;
+}
+
+const citiesArr = [];
+getPromise().then( (data) => citiesArr.push(...formatObjectNumber(data)) );
 const searchInput = document.querySelector('#city-search');
 
 searchInput.addEventListener('change', function() {
-	const filteredData = findMatches(data, this.value.toLowerCase());
+	const filteredData = findMatches(citiesArr, this.value.toLowerCase());
 	displayResults(filteredData, this);
 });
 
 searchInput.addEventListener('keyup', function() {
-	const filteredData = findMatches(data, this.value.toLowerCase());
+	const filteredData = findMatches(citiesArr, this.value.toLowerCase());
 	displayResults(filteredData, this);
 });
