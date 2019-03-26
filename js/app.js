fd76da86d9f13cba2eed568b1529e93779544325
maaajo <matemajch@gmail.com>
2019-03-26 19:28:46 +0100

    smaal adjustments to css and fixed the highlighting regardless of case type

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
-"use strict";
+'use strict';
 
 async function getPromise() {
 	const endpoint =
-		'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';	
+		'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 	const response = await fetch(endpoint);
 	const data = await response.json();
 	return data;
@@ -16,21 +16,25 @@ function findMatches(data, subset) {
 	});
 }
 
-function removeAllChildrens(element) {
+function removeAllChildren(element) {
 	[ ...element.childNodes ].forEach((item) => item.remove());
+}
+
+function createHighlightOfSubstring(substring, fullText) {
+	const regEx = new RegExp(substring, 'gi');
+	return fullText.replace(regEx, (str) => '<span class="highlight">' + str + '</span>');
 }
 
 function displayResults(filteredData, element) {
 	const list = document.querySelector('.suggestions');
 	const hitsArr = [];
 	if (!element.value) {
-		removeAllChildrens(element);
+		removeAllChildren(element);
 	} else {
 		filteredData.forEach(function(item) {
-			const regEx = new RegExp(element.value, 'gi');
 			const cityState = `${item.city}, ${item.state}`;
-			const highlight = cityState.replace(regEx, '<span class="highlight">' + element.value + '</span>');
-			const row = `<li class="suggestion"><span>${highlight}</span><span>${item.population}</span></li>`;
+			const highlightedText = createHighlightOfSubstring(element.value, cityState);
+			const row = `<li class="suggestion"><span>${highlightedText}</span><span>${item.population}</span></li>`;
 			hitsArr.push(row);
 		});
 	}
@@ -43,12 +47,14 @@ function formatNumber(number) {
 }
 
 function formatObjectNumber(data) {
-	data.forEach( function(item) {item.population = formatNumber(+item.population)});
+	data.forEach(function(item) {
+		item.population = formatNumber(+item.population);
+	});
 	return data;
 }
 
 const citiesArr = [];
-getPromise().then( (data) => citiesArr.push(...formatObjectNumber(data)) );
+getPromise().then((data) => citiesArr.push(...formatObjectNumber(data)));
 const searchInput = document.querySelector('#city-search');
 
 searchInput.addEventListener('change', function() {
